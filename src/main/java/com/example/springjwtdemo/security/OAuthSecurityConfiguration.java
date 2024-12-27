051014abe083f26c284337bd8cff9893760906d6
vrameshanand <ramesh.anand@hcl.com>
2024-12-27 06:01:27 -0600

    add jwt to endpoint

--- src/main/java/com/example/springjwtdemo/security/OAuthSecurityConfiguration.java
+++ src/main/java/com/example/springjwtdemo/security/OAuthSecurityConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -40,6 +41,7 @@ public class OAuthSecurityConfiguration {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         http.csrf().disable();
+     http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return (SecurityFilterChain) http.build();
     }
 
